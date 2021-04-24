2622ba5a6cb443094216cd0bcabb41ea1c624213
luncliff <dong-ha-park@linecorp.com>
2021-04-25 01:41:37 +0900

    [mnn] extract cmakelist patch

--- ports/mnn/portfile.cmake
+++ ports/mnn/portfile.cmake
@@ -9,14 +9,12 @@ vcpkg_from_github(
     REF 1.1.6
     SHA512 bdeef79fd5fd044cb5400a56acd39b183d44a3a03f3e7db0e67577c85bfb803b2b840a81480867f35b61888cecf3f0e7d3c32e56b509e41235558108a1debc9b
     HEAD_REF master
-    # PATCHES
-    #     use-package-and-install.patch
+    PATCHES
+        fix-cmakelists.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
   FEATURES
-    test        MNN_BUILD_TEST
-    test        MNN_BUILD_BENCHMARK
     cuda        MNN_CUDA
     cuda        MNN_GPU_TRACE
     tensorrt    MNN_TENSORRT
@@ -30,23 +28,15 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     metal       MNN_METAL
     metal       MNN_GPU_TRACE
     opengl      MNN_OPENGL
-    opengl      MNN_OPENGL_REGEN
     opengl      MNN_USE_SYSTEM_LIB
-    tools       MNN_BUILD_TOOLS
-    tools       MNN_BUILD_QUANTOOLS
-    tools       MNN_BUILD_TRAIN
-    tools       MNN_EVALUATION
-    tools       MNN_BUILD_CONVERTER
-    # codegen     MNN_BUILD_CODEGEN # requires llvm
-    # codegen     MNN_CODEGEN_C
-    # opencl      MNN_CODEGEN_OPENCL
-    opencv      MNN_USE_OPENCV
 )
-
-if(VCPKG_TARGET_IS_WINDOWS)
-    string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" USE_RUNTIME_MT)
-    list(APPEND PLATFORM_OPTIONS -DMNN_WIN_RUNTIME_MT=${USE_RUNTIME_MT})
+if("opengl" IN_LIST FEATURES)
+    if(NOT (VCPKG_TARGET_IS_ANDROID OR VCPKG_TARGET_IS_WINDOWS))
+        message(FATAL_ERROR "The feature 'opengl' requires EGL")
+    endif()
 endif()
+
+vcpkg_find_acquire_program(PYTHON3)
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
 
@@ -54,59 +44,37 @@ vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        ${FEATURE_OPTIONS} ${PLATFORM_OPTIONS}
+        ${FEATURE_OPTIONS}
+        -DPYTHON_EXECUTABLE=${PYTHON3}
         -DMNN_BUILD_SHARED_LIBS=${BUILD_SHARED}
         -DMNN_USE_SSE=ON -DMNN_AVX512=ON -DMNN_SUPPORT_BF16=OFF
         -DMNN_USE_LOGCAT=${VCPKG_TARGET_IS_ANDROID}
         # 1.1.6.0-${commit}
         -DMNN_VERSION_MAJOR=1 -DMNN_VERSION_MINOR=1 -DMNN_VERSION_PATCH=6 -DMNN_VERSION_BUILD=0 -DMNN_VERSION_SUFFIX=-3dada34
+        -DMNN_OPENGL_REGEN=OFF
     OPTIONS_DEBUG
         -DMNN_TRAIN_DEBUG=ON
         -DMNN_DEBUG_MEMORY=ON -DMNN_DEBUG_TENSOR_SIZE=ON
 )
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/MNN TARGET_PATH share/MNN)
-
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/MNN TARGET_PATH share/${PORT})
 
 vcpkg_download_distfile(COPYRIGHT_PATH
     URLS "https://apache.org/licenses/LICENSE-2.0.txt" 
     FILENAME 98f6b79b778f7b0a1541.txt
     SHA512 98f6b79b778f7b0a15415bd750c3a8a097d650511cb4ec8115188e115c47053fe700f578895c097051c9bc3dfb6197c2b13a15de203273e1a3218884f86e90e8
 )
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/${PORT})
 file(RENAME ${COPYRIGHT_PATH} ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
 
 if(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
     if("metal" IN_LIST FEATURES)
         file(RENAME ${CURRENT_PACKAGES_DIR}/bin/mnn.metallib
-                    ${CURRENT_PACKAGES_DIR}/share/${PORT}/mnn.metallib)
+                    ${CURRENT_PACKAGES_DIR}/lib/mnn.metallib)
     endif()
-else()
-    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/${PORT})
 endif()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-if("test" IN_LIST FEATURES)
-    # no install(TARGETS) for the following binaries. check the buildtrees...
-    # vcpkg_copy_tools(
-    #     TOOL_NAMES run_test.out benchmark.out benchmarkExprModels.out # test/
-    #     AUTO_CLEAN
-    # )
-endif()
-if("tools" IN_LIST FEATURES)
-    vcpkg_copy_tools(
-        TOOL_NAMES MNNV2Basic.out mobilenetTest.out backendTest.out testModel.out testModelWithDescrisbe.out getPerformance.out checkInvalidValue.out timeProfile.out # tools/cpp
-                   quantized.out # tools/quantization
-                   classficationTopkEval.out # tools/evaluation
-                   MNNDump2Json MNNConvert # tools/converter
-                   transformer.out train.out dataTransformer.out runTrainDemo.out # tools/train
-        AUTO_CLEAN
-    )
-    if(BUILD_SHARED)
-        vcpkg_copy_tools(TOOL_NAMES TestConvertResult AUTO_CLEAN) # tools/converter
-    endif()
-endif()
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     # remove the others. ex) mnn.metallib
     file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin
