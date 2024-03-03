4ddaf5c359e5d659e850ffed257b4c72e0d38fe3
luncliff <luncliff@gmail.com>
2024-03-03 22:55:41 +0900

    [nvidia-cutlass] create a new port

---
+++ ports/nvidia-cutlass/portfile.cmake
@@ -0,0 +1,45 @@
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO NVIDIA/cutlass
+    REF v3.4.1
+    SHA512 c2ff60af28de951cf4420b163ba2dfc46d30c98fe9e6e765cd1e0be89bf9292e057542ec7061c043c42225b74d970f95f675d366db64105a5c103bb165183ab5
+    HEAD_REF main
+)
+
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON_PATH "${PYTHON3}" PATH)
+vcpkg_add_to_path(PREPEND "${PYTHON_PATH}")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DCUTLASS_REVISION:STRING=v3.4.1
+        -DCUTLASS_ENABLE_HEADERS_ONLY=ON
+        -DCUTLASS_ENABLE_LIBRARY=ON
+        -DCUTLASS_ENABLE_EXAMPLES=OFF
+        -DCUTLASS_ENABLE_PERFORMANCE=OFF
+        -DCUTLASS_LIBRARY_OPERATIONS:STRING=all
+        -DCUTLASS_LIBRARY_KERNELS:STRING=all
+        -DCUTLASS_ENABLE_TOOLS=OFF
+        -DCUTLASS_ENABLE_PROFILER=OFF
+        -DCUTLASS_ENABLE_TESTS=OFF
+        -DCUTLASS_ENABLE_GTEST_UNIT_TESTS=OFF
+        -DCUTLASS_INSTALL_TESTS=OFF
+        -DPython3_EXECUTABLE:FILEPATH=${PYTHON3}
+    MAYBE_UNUSED_VARIABLES
+        CUTLASS_LIBRARY_OPERATIONS
+)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/NvidiaCutlass" PACKAGE_NAME "NvidiaCutlass")
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug"
+    "${CURRENT_PACKAGES_DIR}/test"
+    "${CURRENT_PACKAGES_DIR}/lib"
+)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
